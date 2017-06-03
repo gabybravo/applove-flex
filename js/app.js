c8e740bca516917915cdb23334c225e797d56ebe
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-06-03 13:17:21 -0400

    Ejercicio guiado y evaluado terminados.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+(function modalImages(){
+ 	var proyImagenes = Array.from(document.getElementsByClassName("img-work"));
+ 	var modal = document.getElementById("img-work-modal");
+
+ 	var cuerpoModal, equis, icono;
+
+
+ 	proyImagenes.forEach(function(c){
+ 		c.addEventListener("click",function(){
+ 			modal.innerHTML="";
+ 			cuerpoModal = document.createElement("div");
+ 			cuerpoModal.classList.add("modal-completo");
+
+ 			cuerpoModal.innerHTML = c.innerHTML;
+
+ 			modal.appendChild(cuerpoModal);
+ 			modal.classList.remove("hide");
+
+ 			texto = Array.from(document.getElementsByClassName("n-producto"));
+ 			texto.forEach(function(b){
+ 				b.classList.add("hide");
+ 			}); 			
+			
+ 			equis = document.createElement("div"); 
+ 			equis.classList.add("equis-img");
+ 			icono = document.createElement("i");
+ 			icono.setAttribute("class","fa fa-times");
+ 			icono.setAttribute("aria-hidden","true");
+
+ 			equis.appendChild(icono);
+ 			modal.appendChild(equis);
+
+ 			equis.addEventListener("click", function(){
+ 				modal.classList.add("hide");
+ 			});
+
+ 		});
+		
+ 	});
+ })();
+
+
+
